15b760959707fc1e18f21d58fc15c2de405adcce
SonrieYi <49624511+1D95AEF609@users.noreply.github.com>
2022-10-13 18:35:39 -0600

    actulizacion

--- main.py
+++ main.py
@@ -2,22 +2,17 @@
 from juego import *
 from aplicacion import *
 from manzana import *
-from jugador import *
-
-
-
-
-
+from jugador import Jugador
 
 
 
 
 # Nota solo por el momento esta el menu todavia falta agregar lo de velociodad
 
-
-
 while True:
-
+    print('Bienvenido al juego de Snake')
+    print('-----Menú-----')
+    print('____________________________________')
     print('Niveles de dificultad:')
     print('1- Facil: ')
     print('2- Intermedio:')
@@ -28,6 +23,8 @@ while True:
     if opcion == 1:
         aplicacion = Aplicacion()
         aplicacion.on_execute()
+        Jugador.update()
+
     elif opcion == 2:
         aplicacion = Aplicacion()
         aplicacion.on_execute()
